30a769d194118c333aa96e8c9064d1f499b10391
isaka-12 <isakamtweve69@gmail.com>
2025-11-10 12:48:22 +0300

    New: Initital PyPI settings

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='devforge',
+    name='devforge-cli',  # Changed to avoid name conflict
     version='1.0.0',
     description='Universal project scaffolder for React, FastAPI, and Flutter',
     long_description=long_description,
